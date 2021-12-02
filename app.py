568a562415c147cefe2b80939e44917585d9c1bd
Talha-Altair <cooltalha27@gmail.com>
2021-12-02 19:45:29 +0530

    Updates for 2021-12-02 19:45:29

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 @app.route("/", methods = ["GET","POST"])
 def home():
 
-    data = os.environ
+    data = dict(os.environ)
 
     return jsonify(data)
 
 if __name__ == "__main__":
 
-    app.run(port=8500)
+    app.run(port=8500, host="0.0.0.0", debug=True)
